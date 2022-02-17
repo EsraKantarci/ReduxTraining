7b4a8c64a96cd2aa1e9337d8fe4e30365b47a474
ESRA <av.esrakantarci@gmail.com>
2022-02-17 21:55:40 +0300

    Standart rcc

---
+++ src/components/categories/CategoryList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class categoryList extends Component {
+  render() {
+    return (
+      <div>categoryList</div>
+    )
+  }
+}
---
+++ src/components/root/App.js
@@ -0,0 +1,13 @@
+import Navi from "../navi/Navi";
+import {Container} from "reactstrap"; 
+
+function App() {
+  return <div>
+    <Container>
+    <Navi/>
+    </Container>
+
+  </div>;
+}
+
+export default App;
