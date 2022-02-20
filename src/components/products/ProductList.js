07e2e840df1ab182744a03643dee0cac2c006df1
ESRA <av.esrakantarci@gmail.com>
2022-02-20 19:46:38 +0300

    product list badge

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,9 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Badge } from "reactstrap";
 
-export default class ProductList extends Component {
+class ProductList extends Component {
   render() {
     return (
-      <div><h3>ProductList</h3></div>
-    )
+      <div>
+        <h3>
+          ProductList
+          <Badge color="success">
+            {this.props.currentCategory.categoryName}
+          </Badge>
+        </h3>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer, //proplara bağla
+  };
+}
+
+export default connect(mapStateToProps)(ProductList);
