89be56c48dfef096aeb45e2a396ad1719515dade
ESRA <av.esrakantarci@gmail.com>
2022-02-20 19:34:57 +0300

    category name list group

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,31 +1,37 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { ListGroup, ListGroupItem } from "reactstrap";
 import { bindActionCreators } from "redux";
 import * as categoryActions from "../../redux/actions/categoryActions";
 
 class CategoryList extends Component {
-  
   componentDidMount() {
-    this.props.actions.getCategories();
+    this.props.actions.getCategories(); //uygulama açıldığında state değişecek, kategorileri çekecek
   }
-
 
   render() {
     //redux thunk kurmayı unutma middleware için. configureStore'da importla
     return (
       <div>
         <h2>CategoryList: {this.props.categories.length} </h2>
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem key={category.id}>
+              
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
         <h5>Seçili kategori: {this.props.currentCategory.categoryName}</h5>
-      </div> //map ettim state'ten proplara
+      </div>
     );
   }
 }
 
-
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
-    categories: state.categoryListReducer
+    categories: state.categoryListReducer, //proplara bağla
   };
 }
 
@@ -39,12 +45,9 @@ function mapDispatchToProps(dispatch) {
       changeCategory: bindActionCreators(
         categoryActions.changeCategory,
         dispatch
-      )
-    }
+      ),
+    },
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CategoryList);
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
