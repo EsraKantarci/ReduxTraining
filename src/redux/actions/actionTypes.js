82846b812206bd92342531e7ed33da7c17184767
ESRA <av.esrakantarci@gmail.com>
2022-02-20 17:09:50 +0300

    getCategories debugged at last

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,2 +1,2 @@
-export const CHANGE_CATEGORY = "CHANGE_CATEGORY"
-export const GET_CATEGORIES_SUCCESS = "GET_CATEGORIES_SUCCESS" //api'ye bağlanılarak yapılan async işlemler için redux'un drawback çözümü için redux-tank yapacağız
+export const CHANGE_CATEGORY="CHANGE_CATEGORY"
+export const GET_CATEGORIES_SUCCESS="GET_CATEGORIES_SUCCESS"
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -4,3 +4,4 @@ export default {
     categories: ["A", "B"], //array
   
 };
+
